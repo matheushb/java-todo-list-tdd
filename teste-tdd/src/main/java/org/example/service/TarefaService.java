733d4f1133fbc0e30088153aa869e870a3824bab
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-11-10 17:15:42 -0300

    feat: implemented tarefaService

--- teste-tdd/src/main/java/org/example/service/TarefaService.java
+++ teste-tdd/src/main/java/org/example/service/TarefaService.java
@@ -1,4 +1,29 @@
 package org.example.service;
 
+import org.example.domain.Tarefa;
+import org.example.repositories.TarefaRepository;
+
+import java.util.List;
+
 public class TarefaService {
+
+    TarefaRepository tarefaRepository;
+    public TarefaService(TarefaRepository tarefaRepository) {
+    }
+
+    public void create(Tarefa tarefa) {
+
+    }
+
+    public List<Tarefa> find() {
+    }
+
+    public Tarefa findOne(Long id) {
+    }
+
+    public void update(Long id, String property, String change) {
+    }
+
+    public void delete(long l) {
+    }
 }
