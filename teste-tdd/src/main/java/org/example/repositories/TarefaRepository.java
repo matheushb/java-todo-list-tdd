b5f4ae3c2bfc00ac45019e543228eb68d34c13b6
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-11-10 17:16:07 -0300

    feat: implemented TarefaRepository

---
+++ teste-tdd/src/main/java/org/example/repositories/TarefaRepository.java
@@ -0,0 +1,33 @@
+package org.example.repositories;
+
+import org.example.domain.Tarefa;
+
+import java.util.List;
+
+public class TarefaRepository {
+
+    private List<Tarefa> tarefaList;
+    public TarefaRepository(List<Tarefa> tarefaList) {
+        this.tarefaList = tarefaList;
+    }
+
+    public void create(Tarefa tarefa) {
+
+    }
+
+    public Tarefa update(Long id) {
+
+    }
+
+    public Tarefa delete() {
+
+    }
+
+    public List<Tarefa> find() {
+
+    }
+
+    public Tarefa findOne() {
+
+    }
+}
