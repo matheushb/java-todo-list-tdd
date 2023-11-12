559f2b7e77921ad86deb0e090e59dd070952b6ff
Matheus Baraldi <matheushbaraldi@gmail.com>
2023-11-12 13:04:12 -0300

    feat: implemented TarefaRepository methods

--- teste-tdd/src/main/java/org/example/repositories/TarefaRepository.java
+++ teste-tdd/src/main/java/org/example/repositories/TarefaRepository.java
@@ -3,6 +3,7 @@ package org.example.repositories;
 import org.example.domain.Tarefa;
 
 import java.util.List;
+import java.util.Optional;
 
 public class TarefaRepository {
 
@@ -11,23 +12,52 @@ public class TarefaRepository {
         this.tarefaList = tarefaList;
     }
 
-    public void create(Tarefa tarefa) {
-
+    public boolean create(Tarefa tarefa) throws Exception {
+        try{
+            tarefaList.add(tarefa);
+        }catch(Exception err) {
+            return false;
+        }
+        return true;
     }
 
-    public Tarefa update(Long id) {
+    public Tarefa update(Integer id, Tarefa tarefa) {
 
+        Tarefa tarefaToUpdate = findOne(id);
+
+        if(tarefaToUpdate == null) return null;
+
+        int tarefaIndex = tarefaList.indexOf(tarefaToUpdate);
+
+        if(tarefa.getNome() != null) {
+            tarefaToUpdate.setNome(tarefa.getNome());
+        }
+        if(tarefa.getStatus() != null) {
+            tarefaToUpdate.setStatus(tarefa.getStatus());
+        }
+
+        if(tarefa.getDescricao() != null) {
+            tarefaToUpdate.setDescricao(tarefa.getDescricao());
+        }
+
+        tarefaList.set(tarefaIndex, tarefaToUpdate);
+        return tarefaToUpdate;
     }
 
-    public Tarefa delete() {
 
+    public boolean delete(Integer id) {
+        Tarefa tarefa = findOne(id);
+        if(tarefa == null) return false;
+        tarefaList.remove(tarefa);
+        return true;
     }
 
     public List<Tarefa> find() {
-
+        return tarefaList;
     }
 
-    public Tarefa findOne() {
-
+    public Tarefa findOne(Integer id) {
+        Optional<Tarefa> foundTarefa = tarefaList.stream().filter(tarefa -> tarefa.getId().equals(id)).findFirst();
+        return foundTarefa.orElse(null);
     }
 }
